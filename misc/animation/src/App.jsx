3856c9264ebfe0969e7a2577c71109f6942eb514
Turbobot <turbobot@vercel.com>
2024-03-28 11:20:51 +0530

    feat(create-turbo): apply official-starter transform

---
+++ misc/animation/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import {motion} from "framer-motion"
+
+function App() {
+  
+  
+  return (
+    <> 
+   
+    </>
+  )
+}
+
+export default App
--- week-12/12-2/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function sum(user1, user2) {
-    return user1.age + user2.age;
-}
-console.log(sum({ name: "taro", age: 21 }, { name: "paro", age: 24 }));
